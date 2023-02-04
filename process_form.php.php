7940d31d80f7655f72811b4ebb621536bcbf7db0
samyarj <112454395+samyarj@users.noreply.github.com>
2023-02-04 18:42:44 -0500

    Create process_form.php.php

---
+++ process_form.php.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  
+  // Do something with the form data, such as saving it to a database
+  // ...
+
+  echo "Username: $username<br>";
+  echo "Email: $email";
+}
+?>
