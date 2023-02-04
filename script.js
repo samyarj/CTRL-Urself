d11c2ecfe0dda96360820da55cb3c9e0a9decd47
samyarj <112454395+samyarj@users.noreply.github.com>
2023-02-04 16:31:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const form = document.getElementById("form");
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('nav');
+// get the form element
+var form = document.getElementById("formId").submit();
 
-menuToggle.addEventListener('click', function() {
-  navigation.classList.toggle('menu-active');
+// attach a submit event handler to the form
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // get all the input elements in the form
+  var inputs = form.elements;
+
+  // loop through the input elements and log their values
+  for (var i = 0; i < inputs.length; i++) {
+    console.log(inputs[i].name + ": " + inputs[i].value);
+  }
 });
