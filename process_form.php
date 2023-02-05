c4c3fc93e871bcd2623cc1c8031a9e9f0c541c11
HeyRayKane <112416526+HeyRayKane@users.noreply.github.com>
2023-02-05 01:28:37 -0500

    ajout de fonction pour choix multiple

--- process_form.php
+++ process_form.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
+  
 <?php
 if (isset($_GET)) 
 {
-function IsChecked($chkname,$value)
+function IsChecked($chkname, $value)
     {
         if(!empty($_GET[$chkname]))
         {
@@ -15,6 +17,30 @@ function IsChecked($chkname,$value)
         }
         return false;
     }
+
+function checkMultiple($question)
+  {
+    $choixMultiple = $_GET[$question];
+    $liste = array();
+    $compteur = 1;
+    
+    foreach($choixMultiple as $reponse)
+    {
+      if(IsChecked($question, "REPONSE " . strval($compteur)))
+    {  
+    array_push($liste, 1);
+    $compteur++;
+    } 
+      else 
+      {
+        array_push($liste, 0);
+        $compteur++;
+      }
+    }
+  return $liste; //permet de retourner une liste de booléens pour identifier les sélections
+  }
+
+  
 function checkSelection($entree)
   {
     $question = false;
@@ -28,33 +54,12 @@ function checkSelection($entree)
   return $question;
   }
 
-$reponse1 = false;
-$reponse2 = false;
-$reponse3 = false;
-$reponse4 = false;
-  
-if(IsChecked("question1",'REPONSE 1'))
-{
-    $reponse1 = true;
-}  
-if(IsChecked("question1",'REPONSE 2'))
-{
-    $reponse2 = true;
-} 
-if(IsChecked("question1",'REPONSE 3'))
-{
-    $reponse3 = true;
-} 
-  if(IsChecked("question1",'REPONSE 4'))
-{
-    $reponse4 = true;
-}  
-
+$question1 = checkMultiple("question1");
 $question2 = checkSelection("question2");
+$question3 = checkSelection("question3");
+$question4 = checkSelection("question4"); 
+$question5 = checkSelection("question5"); 
+$question6 = checkSelection("question6");
 
-  $question3 = checkSelection("question3");
-  $question4 = checkSelection("question4"); 
-  $question5 = checkSelection("question5"); 
-  $question6 = checkSelection("question6");
 }
 ?>
