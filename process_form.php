1d9fa631ce2950c4df6c5128aa7ec44d43c9d00b
HeyRayKane <112416526+HeyRayKane@users.noreply.github.com>
2023-02-05 04:08:30 -0500

    Complétion des fonctions

--- process_form.php
+++ process_form.php
@@ -3,63 +3,34 @@
 <?php
 if (isset($_GET)) 
 {
-function IsChecked($chkname, $value)
-    {
-        if(!empty($_GET[$chkname]))
-        {
-            foreach($_GET[$chkname] as $chkval)
-            {
-                if($chkval == $value)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
-function checkMultiple($question)
-  {
-    $choixMultiple = $_GET[$question];
-    $liste = array();
-    $compteur = 1;
-    
-    foreach($choixMultiple as $reponse)
-    {
-      if(IsChecked($question, "REPONSE " . strval($compteur)))
-    {  
-    array_push($liste, 1);
-    $compteur++;
-    } 
-      else 
-      {
-        array_push($liste, 0);
-        $compteur++;
-      }
+function recommanderMul($question) {
+  if (isset($_GET[$question])) {
+    $selected = $_GET[$question];
+    foreach ($selected as $recommandation) {
+      echo $recommandation . " <br>";
     }
-  return $liste; //permet de retourner une liste de booléens pour identifier les sélections
   }
+}
 
-  
-function checkSelection($entree)
+function recommanderUni($question)
   {
-    $question = false;
-    if (isset($_GET[$entree]))
+    if (isset($_GET[$question]))
     {
-      if ($_GET[$entree] == "REPONSE 3" || $_GET[$entree] == "REPONSE 4")
+      if (!($_GET[$question] == "REPONSE 1" || $_GET[$question] == "REPONSE 2"))
       {
-        $question = true;
+        echo $_GET[$question];
       }
     }
   return $question;
   }
 
-$question1 = checkMultiple("question1");
-$question2 = checkSelection("question2");
-$question3 = checkSelection("question3");
-$question4 = checkSelection("question4"); 
-$question5 = checkSelection("question5"); 
-$question6 = checkSelection("question6");
+recommanderMul("question1");
+recommanderUni("question2");
 
+}
+else
+{
+  header("Location: test.html");
 }
 ?>
