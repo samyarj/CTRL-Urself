a00978653e38b2952d6f9d9adad90bf58b3b784e
samyarj <112454395+samyarj@users.noreply.github.com>
2023-02-05 02:29:26 -0500

    adding new file

---
+++ phpWriter.php
@@ -0,0 +1,31 @@
+<?php
+$filename = 'recommendation.html';
+
+// add the new content here:
+$somecontent = "Add this to the file\n";
+
+// Let's make sure the file exists and is writable first.
+if (is_writable($filename)) {
+
+    // In our example we're opening $filename in append mode.
+    // The file pointer is at the bottom of the file hence
+    // that's where $somecontent will go when we fwrite() it.
+    if (!$fp = fopen($filename, 'a')) {
+         echo "Cannot open file ($filename)";
+         exit;
+    }
+
+    // Write $somecontent to our opened file.
+    if (fwrite($fp, $somecontent) === FALSE) {
+        echo "Cannot write to file ($filename)";
+        exit;
+    }
+
+    echo "Success, wrote ($somecontent) to file ($filename)";
+
+    fclose($fp);
+
+} else {
+    echo "The file $filename is not writable";
+}
+?>
